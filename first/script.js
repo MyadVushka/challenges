ea30d759a8946ae840554d96820521ff92ea26f8
VUSZKA <paolini.by@gmail.com>
2023-12-04 18:43:41 +0300

    add an adaptive

---
+++ first/script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let faqs = document.querySelectorAll('.front-block__lower_button');
+
+faqs.forEach((faq) => {
+  faq.addEventListener('click', function() {
+
+      let panel = faq.nextElementSibling;
+      let icon = faq.querySelector('.front-block__lower-icon');
+      
+
+      
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+        icon.src = './imgs/icon-plus.svg';
+      }
+      else {
+        panel.style.display = 'block';
+        icon.src = './imgs/icon-minus.svg';
+      }
+  })
+})
