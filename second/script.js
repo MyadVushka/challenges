b246cdcbd53af80bfc27969466c8ec5ecd3fac53
VUSZKA <paolini.by@gmail.com>
2023-12-08 14:56:46 +0300

    'add: add a new challange task'

---
+++ second/script.js
@@ -0,0 +1,47 @@
+'use strict'
+
+let stars = document.querySelectorAll('.wrapper__marks_mod')
+
+
+let starActive;
+
+stars.forEach((star, idx) => {
+
+  star.addEventListener('click', function() {
+
+    if (starActive === undefined) {
+      starActive = idx;
+      star.style.backgroundColor = 'hsl(216, 12%, 54%)';
+    }
+    else {
+      stars[starActive].style.backgroundColor = 'rgba(255, 255, 255, 0.063)';
+      star.style.backgroundColor = 'hsl(216, 12%, 54%)';
+      starActive = idx;
+    }
+  })
+
+  star.addEventListener('mouseenter', function() {
+    if (starActive !== idx) {
+      star.style.backgroundColor = 'hsl(25, 97%, 53%)';
+    }
+  })
+  star.addEventListener('mouseout', function() {
+    if (starActive !== idx) {
+      star.style.backgroundColor = 'rgba(255, 255, 255, 0.063)';
+    }
+  })
+})
+
+let submit = document.querySelector('.wrapper__button');
+let wrap1 = document.getElementById('wrap1');
+let wrap2 = document.getElementById('wrap2');
+
+submit.addEventListener('click', function() {
+  wrap1.style.display = 'none';
+  wrap2.style.display = 'inline-flex';
+})
+
+
+
+
+
