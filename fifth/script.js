c8a172da66d1ed88fb41da764d5160e296edd0b6
VUSZKA <paolini.by@gmail.com>
2024-01-16 20:14:41 +0300

    feat: add sync cardnumber and name

--- fifth/script.js
+++ fifth/script.js
@@ -1,1 +1,34 @@
-    
+const inputFrom = document.querySelector('#cardNumberGeneral');
+const inputTo = document.querySelector('.cardnumber');
+const nameFrom = document.querySelector('#nameSurnameFrom');
+const nameTo = document.querySelector('.nameSurname');
+const dateTo = document.querySelector('.date');
+const dateFrom = document.querySelectorAll('.input.second');
+
+function transferInputs() {
+
+  let formatedInput = '';
+
+  for (let i = 0; i < inputFrom.value.length; i++) {
+    i % 4 == 0 ? formatedInput += ' ' + inputFrom.value[i] : formatedInput += inputFrom.value[i]; 
+  }
+
+  inputTo.value = formatedInput;
+}
+
+function nameTransferInputs() {
+  nameTo.value = (nameFrom.value).toUpperCase();
+}
+
+function dateTransferInputs() {
+
+  let formattedDate = '';
+
+}
+
+inputFrom.addEventListener('input', transferInputs);
+nameFrom.addEventListener('input', nameTransferInputs);
+
+for (let i = 0; i < dateFrom.length; i++) {
+  dateFrom[i].addEventListener('input', dateTransferInputs);
+}
