81d6936d2057da5bedb1551af313462b50799ddd
VUSZKA <paolini.by@gmail.com>
2024-01-22 15:20:56 +0300

    feat: add the second screen and max-length input property

--- fifth/script.js
+++ fifth/script.js
@@ -4,6 +4,11 @@ const nameFrom = document.querySelector('#nameSurnameFrom');
 const nameTo = document.querySelector('.nameSurname');
 const dateTo = document.querySelector('.date');
 const dateFrom = document.querySelectorAll('.input.second');
+const cvvCardFrom = document.querySelector('.input.third');
+const cvvCardTo = document.querySelector('.cvvInputCard');
+const confirmButton = document.querySelector('.confirmButton');
+const secondScreen = document.querySelector('#wrapper__right2');
+const firstScreen = document.querySelector('#wrapper__right1')
 
 function transferInputs() {
 
@@ -22,13 +27,38 @@ function nameTransferInputs() {
 
 function dateTransferInputs() {
 
-  let formattedDate = '';
+  let formatedDate = '';
 
+  const monthValue = dateFrom[0].value;
+  const yearValue = dateFrom[1].value;
+
+  if (monthValue.length === 2) {
+    formatedDate += monthValue + '/';
+  }
+  else {
+    formatedDate += monthValue;
+  }
+
+  formatedDate += yearValue;
+
+  dateTo.value = formatedDate;
+
+}
+
+function cvvTransferInput() {
+  cvvCardTo.value = cvvCardFrom.value;
 }
 
 inputFrom.addEventListener('input', transferInputs);
 nameFrom.addEventListener('input', nameTransferInputs);
+cvvCardFrom.addEventListener('input', cvvTransferInput);
 
 for (let i = 0; i < dateFrom.length; i++) {
   dateFrom[i].addEventListener('input', dateTransferInputs);
 }
+
+confirmButton.addEventListener('click', function() {
+  firstScreen.style.display = 'none';
+  secondScreen.style.display = 'flex';
+})
+
